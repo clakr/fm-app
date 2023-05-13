20b119a42b0d294f5645dc0ae872c491a21427b5
clakr <clarktolosa@gmail.com>
2023-05-13 21:08:01 +0800

    init

---
+++ README.md
@@ -0,0 +1,2 @@
+# fm-app
+Serves as repository for bootstrapping an application for developing Frontend Mentor challenges
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+
+async function main() {
+  console.log("hello from index.ts! --watch???");
+}
+
+main().then((err) => console.error(err));
